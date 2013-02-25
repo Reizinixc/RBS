cb4462cd5dc3bd9a6311063fa03b548b2e6d1886
Reizinixc <keiseiie_kune@hotmail.co.jp>
2013-02-25 23:31:10 +0700

    Fixed create booking broke link

--- application/views/booking/index.php
+++ application/views/booking/index.php
@@ -2,7 +2,7 @@
   <h2 class="page-header">My Booking List</h2>
 
   <nav class="nav">
-    <a class="btn btn-success" href="<?= site_url('booking/create') ?>"><i class="icon-plus"></i> Create Booking</a>
+    <a class="btn btn-success" href="<?= site_url('bookings/create') ?>"><i class="icon-plus"></i> Create Booking</a>
   </nav>
 
   <?= $this->load->view('booking/bookingList', true) ?>
